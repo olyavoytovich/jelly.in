a195e3d1e8c8391d1569ea1915130e74ca229e87
Kate <katealexposp@gmail.com>
2020-05-17 21:31:28 +0300

    Add levels
    Добавлены новые уровни

--- View/choose_level_menu.cpp
+++ View/choose_level_menu.cpp
@@ -31,7 +31,7 @@ ChooseLevelMenu::ChooseLevelMenu(AbstractGameController* game_controller,
     }
   }
 
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < 7; i++) {
     connect(level_buttons_[i], &QPushButton::clicked, this, [&, i]() {
       game_controller_->StartLevel(i);
     });
